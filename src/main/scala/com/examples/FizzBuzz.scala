02297873f0b6543e2cc011dd0f4ee4e243a86695
MrWalshyType2 <mwalsh@academytrainee.com>
2020-10-21 17:04:22 +0100

    Added two more examples for fizz buzz, both use pattern matching

--- src/main/scala/com/examples/FizzBuzz.scala
+++ src/main/scala/com/examples/FizzBuzz.scala
@@ -27,6 +27,34 @@ object FizzBuzz extends App {
   println(ans6)
   println(ans7)
   println(ans8)
+  println()
+
+  val ans9 = fizzBuzzAlt(-3)
+  val ans10 = fizzBuzzAlt(120)
+  val ans11 = fizzBuzzAlt(5)
+  val ans12 = fizzBuzzAlt(11)
+  val ans13 = fizzBuzzAlt(0)
+
+  println(ans9)
+  println(ans10)
+  println(ans11)
+  println(ans12)
+  println(ans13)
+  println()
+
+  val ans14 = fizzBuzzAlt2(Option(-3))
+  val ans15 = fizzBuzzAlt2(Option(120))
+  val ans16 = fizzBuzzAlt2(Option(5))
+  val ans17 = fizzBuzzAlt2(Option(11))
+  val ans18 = fizzBuzzAlt2(Option(0))
+  val ans19 = fizzBuzzAlt2(None)
+
+  println(ans14)
+  println(ans15)
+  println(ans16)
+  println(ans17)
+  println(ans18)
+  println(ans19)
 
   def fizzBuzz(input: Integer): String = {
     if (input == 0) input.toString
@@ -34,5 +62,25 @@ object FizzBuzz extends App {
     else if ((input % 5) == 0) "buzz"
     else if ((input % 3) == 0) "fizz"
     else input.toString
+  }
+
+  def fizzBuzzAlt(input: Integer): String = {
+    if (input == 0) input.toString
+    // Creating a tuple with the results of the ops, then using tuples with expected results to decide the return val
+    else (input % 3, input % 5) match {
+      case (0, 0) => "fizzbuzz"
+      case (_, 0) => "buzz"
+      case (0, _) => "fizz"
+      case _ => input.toString
+    }
+  }
+
+  def fizzBuzzAlt2(option: Option[Int]): String = {
+    option match {
+      case Some(i) => {
+        fizzBuzzAlt(i)
+      }
+      case None => ""
+    }
   }
 }
